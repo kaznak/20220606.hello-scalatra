1b6fd5f777274b66d018520be3db3e4af20de444
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2022-06-13 07:30:30 +0900

    src/main/scala/ScalatraBootstrap.scala: use ServerConfig

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -2,8 +2,11 @@ import com.github.kaznak._
 import org.scalatra._
 import javax.servlet.ServletContext
 
+import com.github.kaznak.util.Config
+
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext): Unit = {
-    context.mount(new BlogServlet, "/*")
+    val serverConfig = Config.load(None)
+    context.mount(new BlogServlet(serverConfig), "/*")
   }
 }
