047547c6d4b9efa3e61b7f2a1bb0220e5e48bf40
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2022-06-13 07:29:58 +0900

    src/main/scala/com/github/kaznak/BlogServlet.scala: use ServerConfig

--- src/main/scala/com/github/kaznak/BlogServlet.scala
+++ src/main/scala/com/github/kaznak/BlogServlet.scala
@@ -1,15 +1,18 @@
 package com.github.kaznak
 
 import org.scalatra._
+import com.github.kaznak.util.ServerConfig
 
-class BlogServlet extends ScalatraServlet {
+class BlogServlet(
+    val serverConfig: ServerConfig
+) extends ScalatraServlet {
 
   get("/") {
     views.html.hello()
   }
 
   get("/hi-there") {
-    "Hi there!"
+    serverConfig.hiThereMessage
   }
 
 }
