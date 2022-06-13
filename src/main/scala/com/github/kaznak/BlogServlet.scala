222a0e835bb2eba21d00843099377be287796db6
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2022-06-13 16:04:06 +0900

    src/main/scala/com/github/kaznak/BlogServlet.scala: use logging facility

--- src/main/scala/com/github/kaznak/BlogServlet.scala
+++ src/main/scala/com/github/kaznak/BlogServlet.scala
@@ -1,11 +1,13 @@
 package com.github.kaznak
 
 import org.scalatra._
+import com.github.kaznak.Logging
 import com.github.kaznak.util.ServerConfig
 
 class BlogServlet(
     val serverConfig: ServerConfig
-) extends ScalatraServlet {
+) extends ScalatraServlet
+    with Logging {
 
   get("/") {
     views.html.hello()
